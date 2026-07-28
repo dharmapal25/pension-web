5844fdbc515f42de148adc4e4a367aef248ad630
dharmapalbharati-web <dharmapalbharati2525@gmail.com>
2026-07-28 21:22:32 +0530

    feat: refactor Firebase admin initialization to use environment variables

--- server/src/config/adminFirebase.js
+++ server/src/config/adminFirebase.js
@@ -1,12 +1,12 @@
-import admin from "firebase-admin";
-import { readFileSync } from "fs";
+import { initializeApp, cert } from "firebase-admin/app";
 
-const serviceAccount = JSON.parse(
-  readFileSync("./serviceAccountKey.json", "utf8")
-);
+const admin = initializeApp({
+  credential: cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  }),
 });
 
 export default admin;
