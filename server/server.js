067b0f34e93c9167cb17fdaeef764cc4ee681868
dharmapalbharati-web <dharmapalbharati2525@gmail.com>
2026-07-28 22:27:12 +0530

    feat: establish MongoDB connection in server initialization

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const app = require("./src/app");
+const mongoDBConnection = require("./src/config/mongodb")
+
 const PORT = process.env.PORT
 
 if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is required to start the server");
 }
+
+mongoDBConnection();
 
 app.listen(PORT,()=> {
     console.log(`Server is running on PORT ${PORT}`);
